16d4dcf707bf90031493e7294d6383606338a5bc
TechEdzprogrammer <113817954+TechEdzprogrammer@users.noreply.github.com>
2025-03-21 23:38:21 +0800

    Remove strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,7 @@ import App from './App.jsx'
 import { HashRouter} from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <HashRouter>
       <App/>
     </HashRouter>
-  </StrictMode>,
 )
